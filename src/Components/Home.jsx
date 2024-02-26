b029720d1729a971236749a9809495a2adcf6b0c
Abdullah Mokhtar <Abdullahmokhtr55@gmail.com>
2024-02-26 19:54:43 +0200

    video play in mobile

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,11 @@
-import React from "react";
 import vid from "../assets/videos/T-Swiss BG.mp4";
 
 const Home = () => {
   return (
     <section id="home" className="vh-100 pt-0">
       <div className="bg-video">
-        <video autoPlay loop muted>
-          <source src={vid} type="video/mp4" />
+        <video muted={true} autoPlay loop={true} playsInline={true}>
+          <source autoPlay src={vid} type="video/mp4" />
         </video>
       </div>
       <div className="home-txt">
